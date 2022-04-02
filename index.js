25f1e8e991f2ed551c0153a0f127222179aec710
shaunvan119 <shaunvan@live.com.au>
2022-04-02 16:45:18 +1100

    JS button collection event listeners

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const testBtn = document.getElementById("test")
+const washBtn = document.getElementById("wash")
+const mowBtn = document.getElementById("Mow")
+const weedBtn = document.getElementById("Weeds")
+const sendBtn = document.getElementById("send-invoice")
 
-testBtn.addEventListener("click", function() {
+
+washBtn.addEventListener("click", function() {
     console.log("click")
 })
+
+mowBtn.addEventListener("click", function() {
+    console.log("click")
+})
+
+weedBtn.addEventListener("click", function() {
+    console.log("click")
+})
+
+sendBtn.addEventListener("click", function() {
+    console.log("click")
+})
+
+
+
