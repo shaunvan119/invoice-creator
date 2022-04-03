b1bdfa416c3409a79552d4904fff98579b359355
shaunvan119 <shaunvan@live.com.au>
2022-04-03 10:11:46 +1000

    task and total varibles and event listeners

--- index.js
+++ index.js
@@ -3,24 +3,30 @@ const mowBtn = document.getElementById("Mow")
 const weedBtn = document.getElementById("Weeds")
 const sendBtn = document.getElementById("send-invoice")
 
+let totalWashAmount = document.getElementById("wash").value;
+let taskWashAmount = document.getElementById("wash").name
+let totalMowAmount = document.getElementById("Mow").value;
+let taskMowAmount = document.getElementById("Mow").name
+let totalWeedAmount = document.getElementById("Weeds").value;
+let taskWeedAmount = document.getElementById("Weeds").name
 
 
-//function washFunction() {
-    //let totalWashAmount = document.getElementById("wash").value;
-   // document.getElementById("total-wash").innerHTML = totalWashAmount;
-//}
+
 
 washBtn.addEventListener("click", function() {
-    let totalWashAmount = document.getElementById("wash").value;
+    document.getElementById("task-wash-task").innerHTML = taskWashAmount;
     document.getElementById("total-wash").innerHTML = totalWashAmount;
 })
 
 mowBtn.addEventListener("click", function() {
-    console.log("click")
+    document.getElementById("task-mow-task").innerHTML = taskMowAmount;
+    document.getElementById("total-mow").innerHTML = totalMowAmount;
+    
 })
 
 weedBtn.addEventListener("click", function() {
-    console.log("click")
+    document.getElementById("task-weeds-task").innerHTML = taskWeedAmount
+    document.getElementById("total-weeds").innerHTML = totalWeedAmount;
 })
 
 sendBtn.addEventListener("click", function() {
